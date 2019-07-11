95b0133f052a76af412cdfb97cc11e21418cb291
fdr-smnv <49762416+fdr-smnv@users.noreply.github.com>
2019-07-11 16:58:20 +0300

    Creating main server file with initial setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv').config()
+
+const app = express()
+
+const { PORT = 3000 } = process.env
+
+// Middlewares
+
+// Routes
+
+// Start the server 
+
+app.listen(PORT, () => console.log(`Server listening at ${PORT}`))
