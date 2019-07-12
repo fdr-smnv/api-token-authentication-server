f9dda96eccfef347a8a742889b7c977c0fe9ac64
fdr-smnv <49762416+fdr-smnv@users.noreply.github.com>
2019-07-12 22:14:40 +0300

    Added more requirements to USER schema

--- models/user.js
+++ models/user.js
@@ -4,8 +4,16 @@ const Schema = mongoose.Schema
 // User Schema
 
 const userSchema = new Schema({
-    email: String,
-    password: String
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
 })
 
 // User model
