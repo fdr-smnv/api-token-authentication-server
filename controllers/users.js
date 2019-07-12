00c75ec1a26a33059fc85726ab77933129f9eb19
fdr-smnv <49762416+fdr-smnv@users.noreply.github.com>
2019-07-12 22:09:47 +0300

    SignUp route now creates a new user in db

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,27 @@
+const User = require('../models/user')
+
 module.exports = {
     signUp: async (req, res, next) => {
         try {
-            console.log('UsersController.signUp called!')
-            next()
+            const { email, password } = req.value.body
+
+            // Check if there is a user with the same email
+
+            const foundUser = await User.findOne({ email })
+
+            if (foundUser) {
+                return res.status(403).json({error: 'Email is already in use'})
+            }
+
+            // Create a new user
+
+            const newUser = new User({ email, password })
+
+            await newUser.save()
+
+            // Respond with token
+
+            res.json({ user: 'created' })
         } catch (error) {
             next(error)
         }
@@ -10,6 +29,7 @@ module.exports = {
 
     signIn: async (req, res, next) => {
         try {
+            // Generate token
             console.log('UsersController.signIn called!')
             next()
         } catch (error) {
