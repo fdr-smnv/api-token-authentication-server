c830484e1e1ed5d9c007fca8e12a45d11d76ac6c
fdr-smnv <49762416+fdr-smnv@users.noreply.github.com>
2019-07-13 16:21:09 +0300

    Creating passport strategy middleware

---
+++ passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport')
+const { JwtStrategy, ExtractJwt } = require('passport-jwt')
+
+const { User } = require('./models/user')
+const { JWT_SECRET } = require('./helpers/envVariables')
+
+passport.use(new JwtStrategy({
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: JWT_SECRET
+}, async (payload, done) => {
+  try {
+    // Find the user specified in token
+
+    const user = await User.findById(payload.sub)
+
+    // If user doesn't exist, handle it
+
+    if (!user) {
+      return done(null, false)
+    }
+
+    // Otherwise, return the user
+
+    done(null, user)
+
+  } catch (error) {
+    done(error, false)
+  }
+}))
+
