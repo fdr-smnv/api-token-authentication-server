8f69b8b4186fd386ed8eee431e7a872f4c8f9eab
fdr-smnv <49762416+fdr-smnv@users.noreply.github.com>
2019-07-13 15:22:43 +0300

    If there were no errors signUp route responds with JWT token

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const JWT = require('jsonwebtoken')
 const User = require('../models/user')
 
 module.exports = {
@@ -8,20 +9,26 @@ module.exports = {
             // Check if there is a user with the same email
 
             const foundUser = await User.findOne({ email })
-
             if (foundUser) {
-                return res.status(403).json({error: 'Email is already in use'})
+                return res.status(403).json({ error: 'Email is already in use' })
             }
 
             // Create a new user
 
             const newUser = new User({ email, password })
-
             await newUser.save()
 
             // Respond with token
 
-            res.json({ user: 'created' })
+            const token = JWT.sign({
+                iss: 'Issuer',
+                sub: newUser.id,
+                iat: new Date().getTime(), // current time
+                exp: new Date().setDate(new Date().getDate() + 1) // current time + 1 day ahead
+            }, 'secret')
+
+            res.status(200).json({ token })
+
         } catch (error) {
             next(error)
         }
