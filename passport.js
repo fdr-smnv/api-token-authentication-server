3231ee23024f4d207ed865ea3dda31c06235daed
fdr-smnv <49762416+fdr-smnv@users.noreply.github.com>
2019-07-14 14:16:50 +0300

    Passport local strategy setup

--- passport.js
+++ passport.js
@@ -15,17 +15,14 @@ passport.use(new JwtStrategy({
 }, async (payload, done) => {
   try {
     // Find the user specified in token
-
     const user = await User.findById(payload.sub)
 
     // If user doesn't exist, handle it
-
     if (!user) {
       return done(null, false)
     }
 
     // Otherwise, return the user
-
     done(null, user)
 
   } catch (error) {
@@ -39,18 +36,26 @@ passport.use(new LocalStrategy({
   usernameField: 'email'
 }, async (email, password, done) => {
   try {
+    // Find the user given the email
     const user = await User.findOne({ email })
 
+    // If not, handle it
     if (!user) {
       return done(null, false)
     }
 
-    if (password !== user.password) {
-      return done(null, false)
-    } else {
-      return done(null, user)
-    }
-  } catch (error) {
+    // Check if password is correct
+    const isMatch = await user.isValidPassword(password)
 
+    // If not, handle it
+    if (!isMatch) {
+      return done(null, false)
+    }
+
+    // Otherwise, return the user
+    done(null, user)
+
+  } catch (error) {
+    done(error, false)
   }
 }))
