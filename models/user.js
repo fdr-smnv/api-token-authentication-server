d6f0a03c8aeba8ee4ce3670928a15f3f5f4a5f77
fdr-smnv <49762416+fdr-smnv@users.noreply.github.com>
2019-07-14 14:19:49 +0300

    Hashing password before saving it to db and added function that compares hashed password and provided one

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
 
-// User Schema
+// Create a schema
 
 const userSchema = new Schema({
   email: {
@@ -16,7 +17,29 @@ const userSchema = new Schema({
   }
 })
 
-// User model
+// Hashing before saving
+
+userSchema.pre('save', async function (next) {
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10)
+    // Re-assign hashed password over original
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+  } catch (error) {
+    next(error)
+  }
+})
+
+userSchema.methods.isValidPassword = async function (newPassword) {
+  try{
+    return await bcrypt.compare(newPassword, this.password)
+  } catch(error) {
+    throw new Error(error)
+  }
+}
+
+// Create a model
 
 const User = mongoose.model('user', userSchema)
 
