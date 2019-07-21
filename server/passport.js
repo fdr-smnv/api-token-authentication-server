3401e3f42dbd544246187947b0ae76e2d8e36267
fdr-smnv <49762416+fdr-smnv@users.noreply.github.com>
2019-07-21 15:03:59 +0300

    Added Google OAuth signin strategy

--- server/passport.js
+++ server/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const JwtStrategy = require('passport-jwt').Strategy
 const { ExtractJwt } = require('passport-jwt')
 const LocalStrategy = require('passport-local').Strategy
+const GooglePlusTokenStrategy = require('passport-google-plus-token')
 
 const User = require('./models/user')
 const { JWT_SECRET } = require('./helpers/envVariables')
@@ -26,6 +27,40 @@ passport.use(new JwtStrategy({
 
   } catch (error) {
     done(error, false)
+  }
+}))
+
+// GOOGLE OAUTH STRATEGY
+passport.use('googleToken', new GooglePlusTokenStrategy({
+  clientID: '594652903364-uifok1arnekjbdgi15fsmvlng0ta2s79.apps.googleusercontent.com',
+  clientSecret: '1ZmvQYwh5ZQVjQsiQ-fwGui3'
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    console.log('accessToken', accessToken, '\n')
+    console.log('refreshToken', refreshToken)
+    console.log('profile', profile)
+
+    // Check whether this current user exists in out db
+    // If it exists, return this user
+    const existingUser = await User.findOne({ 'google.id': profile.id })
+
+    if (existingUser) {
+      return done(null, existingUser)
+    }
+
+    // If account is new
+    const newUser = new User({
+      method: 'google',
+      google: {
+        id: profile.id,
+        email: profile.emails[0].value
+      }
+    })
+
+    await newUser.save()
+    done(null, newUser)
+  } catch (error) {
+    done(error, false, error.message)
   }
 }))
 
